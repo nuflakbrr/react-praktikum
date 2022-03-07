ae85a26b7a8d43774b498c3fa6a9ae713c6b95e1
Naufal Akbar <kbrnugroho@gmail.com>
2022-03-07 18:38:35 +0700

    create modal component

--- src/components/FormBMI.js
+++ src/components/FormBMI.js
@@ -3,6 +3,7 @@ import 'bootstrap-icons/font/bootstrap-icons.css'
 
 import TextInput from './TextInput'
 import Alert from './Alert'
+import Modal from './Modal'
 
 const FormBMI = () => {
     const [height, setHeight] = useState(1.0)
@@ -22,7 +23,10 @@ const FormBMI = () => {
     return (
         <>
             <div className='card p-3'>
-                <h2 className='text-center fw-bold'>Kalkulator BMI</h2>
+                <div className='d-flex justify-content-center align-items-center'>
+                    <h2 className='text-center fw-bold'>Kalkulator BMI</h2>
+                    <button className='btn btn-dark badge ms-3' data-bs-toggle='modal' data-bs-target='#BMI'><i class='bi bi-info-circle-fill'></i></button>
+                </div>
                 <form onSubmit={calculate}>
                     <div className='form-group mb-3'>
                         <TextInput label='Berat (ex: 60)' type='number' onChange={(e) => setMass(e.target.value)} id='weight' value={mass} placeholder='Silakan masukkan berat anda' min='30' extra='Kg' margin='me-3' />
@@ -45,6 +49,7 @@ const FormBMI = () => {
                                 : <Alert heading='Obesitas' type='danger' icons='bi-exclamation-triangle-fill' />
                 }
             </div>
+            <Modal id='BMI' title='Tentang Kalkulator BMI' content='Kalkulator BMI adalah alat bantu hitung BMI (Body Mass Index). BMI sendiri adalah angka yang menjadi penilaian standar untuk menentukan apakah berat badan Anda tergolong normal, kurang, berlebih, atau obesitas.' />
         </>
     )
 }
