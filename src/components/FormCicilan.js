0c6bc8ebd95ea9bb5c3be732b4cdeb9cacbf8a1e
Naufal Akbar <kbrnugroho@gmail.com>
2022-03-07 18:44:49 +0700

    added modal component

--- src/components/FormCicilan.js
+++ src/components/FormCicilan.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Modal from './Modal'
 
 import TextInput from './TextInput'
 import TextSelect from './TextSelect'
@@ -7,7 +8,10 @@ function FormCicilan() {
     return (
         <>
             <div className='card p-3'>
-                <h2 className='text-center fw-bold'>Kalkulator Cicilan Bank</h2>
+                <div className='d-flex justify-content-center align-items-center'>
+                    <h2 className='text-center fw-bold'>Kalkulator Cicilan Bank</h2>
+                    <button className='btn btn-dark badge ms-3' data-bs-toggle='modal' data-bs-target='#Cicilan'><i class='bi bi-info-circle-fill'></i></button>
+                </div>
                 <form>
                     <div className='form-group mb-3'>
                         <TextInput label='Nominal' type='number' />
@@ -27,6 +31,7 @@ function FormCicilan() {
             <div className='card p-3 my-5'>
                 <h2 className='text-center fw-bold'>Hasil</h2>
             </div>
+            <Modal id='Cicilan' title='Tentang Kalkulator Cicilan Bank' content='Kalkulator CIcilan Bank adalah alat bantu hitung Cicilan Bank (Kredit). Berdasarkan UU No. 10 tahun 1998, kredit bank adalah "penyediaan uang atau tagihan yang dapat dipersamakan dengan itu, berdasarkan persetujuan atau kesepakatan pinjam meminjam antara bank dengan pihak lain yang mewajibkan pihak peminjam untuk melunasi utangnya setelah jangka waktu tertentu dengan pemberian bunga".' />
         </>
     )
 }
