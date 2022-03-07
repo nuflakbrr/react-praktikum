79242b07524f544b72dfe1ee25d6f4492c2d3640
Naufal Akbar <kbrnugroho@gmail.com>
2022-03-07 14:26:43 +0700

    change brand name

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,9 +9,9 @@ const Footer = () => {
     return (
         <div className="container">
             <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
-                <div className="col-md-4 d-flex align-items-center">
+                <div className="col-md-4">
                     <Link to="/" className="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
-                        Naufal Akbar Nugroho
+                        Naufal Akbar
                     </Link>
                     <span className="text-muted">&copy; {year} nuflakbrr</span>
                 </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -15,7 +15,7 @@ const Navbar = () => {
         <div>
             <nav className='navbar navbar-expand-lg navbar-dark bg-dark py-3'>
                 <div className='container'>
-                    <Link className='navbar-brand fw-bold' to='/'>Naufal Akbar Nugroho</Link>
+                    <Link className='navbar-brand fw-bold' to='/'>Naufal Akbar</Link>
                     <button className='navbar-toggler' type='button' data-bs-toggle='collapse' data-bs-target='#navbarNav' aria-controls='navbarNav' aria-expanded='false' aria-label='Toggle navigation'>
                         <span className='navbar-toggler-icon'></span>
                     </button>
