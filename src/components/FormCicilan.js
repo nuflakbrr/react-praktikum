417bd9131ffdb31b34a844d12020cbd87ed7f6de
Naufal Akbar <kbrnugroho@gmail.com>
2022-03-08 07:19:01 +0700

    hard code select component

--- src/components/FormCicilan.js
+++ src/components/FormCicilan.js
@@ -2,7 +2,6 @@ import React from 'react'
 import Modal from './Modal'
 
 import TextInput from './TextInput'
-import TextSelect from './TextSelect'
 
 function FormCicilan() {
     return (
@@ -20,7 +19,25 @@ function FormCicilan() {
                         <TextInput label='Bunga' type='number' margin='me-4' extra='%' />
                     </div>
                     <div className='form-group mb-3'>
-                        <TextSelect label='Periode' margin='me-3' extra='bln' />
+                        <label>Periode</label>
+                        <div className='d-flex align-items-center'>
+                            <select className='form-select me-3'>
+                                <option selected disabled>Pilih Bulan</option>
+                                <option value='1'>1 Bulan</option>
+                                <option value='2'>2 Bulan</option>
+                                <option value='3'>3 Bulan</option>
+                                <option value='4'>4 Bulan</option>
+                                <option value='5'>5 Bulan</option>
+                                <option value='6'>6 Bulan</option>
+                                <option value='7'>7 Bulan</option>
+                                <option value='8'>8 Bulan</option>
+                                <option value='9'>9 Bulan</option>
+                                <option value='10'>10 Bulan</option>
+                                <option value='11'>11 Bulan</option>
+                                <option value='12'>12 Bulan</option>
+                            </select>
+                            <span>bln</span>
+                        </div>
                     </div>
                 </form>
                 <hr />
@@ -31,7 +48,7 @@ function FormCicilan() {
             <div className='card p-3 my-5'>
                 <h2 className='text-center fw-bold'>Hasil</h2>
             </div>
-            <Modal id='Cicilan' title='Tentang Kalkulator Cicilan Bank' content='Kalkulator CIcilan Bank adalah alat bantu hitung Cicilan Bank (Kredit). Berdasarkan UU No. 10 tahun 1998, kredit bank adalah "penyediaan uang atau tagihan yang dapat dipersamakan dengan itu, berdasarkan persetujuan atau kesepakatan pinjam meminjam antara bank dengan pihak lain yang mewajibkan pihak peminjam untuk melunasi utangnya setelah jangka waktu tertentu dengan pemberian bunga".' />
+            <Modal id='Cicilan' title='Tentang Kalkulator Cicilan Bank' content='Kalkulator Cicilan Bank adalah alat bantu hitung Cicilan Bank (Kredit). Berdasarkan UU No. 10 tahun 1998, kredit bank adalah "penyediaan uang atau tagihan yang dapat dipersamakan dengan itu, berdasarkan persetujuan atau kesepakatan pinjam meminjam antara bank dengan pihak lain yang mewajibkan pihak peminjam untuk melunasi utangnya setelah jangka waktu tertentu dengan pemberian bunga".' />
         </>
     )
 }
