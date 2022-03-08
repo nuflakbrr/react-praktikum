16a8e1d219d6a66c81cbde9889baf748717156df
Naufal Akbar <kbrnugroho@gmail.com>
2022-03-08 08:52:01 +0700

    hotfix

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -15,7 +15,7 @@ const Navbar = () => {
                     <button className='navbar-toggler' type='button' data-bs-toggle='collapse' data-bs-target='#navbarNav' aria-controls='navbarNav' aria-expanded='false' aria-label='Toggle navigation'>
                         <span className='navbar-toggler-icon'></span>
                     </button>
-                    <div className='collapse navbar-collapse' id='navbarSupportedContent'>
+                    <div className='collapse navbar-collapse' id='navbarNav'>
                         <ul className='navbar-nav ms-auto'>
                             <li className='nav-item'>
                                 <Link className={`nav-link ${activeClass('/')}`} aria-current='page' to='/'>Beranda</Link>
